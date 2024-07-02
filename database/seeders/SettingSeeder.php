2af27a8e4a1ee0aa57f335a3d089856130e51d11
nadaAbedAlkreem <elkahloutnada@gmail.com>
2024-07-02 17:30:55 +0300

    first commit

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Setting;
 
 class SettingSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class SettingSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Settings::create(['key' => 'logo_header', 'value' =>'', 'type_field'=> 'images' ]);
+        Settings::create(['key' => 'logo_footer', 'value' =>'','type_field'=> 'images' ]);
+        Settings::create(['key' => 'name_website', 'value' =>'' ,'type_field'=> 'string' ]);
+        Settings::create(['key' => 'header_card_top', 'value' =>'' ,'type_field'=> 'string' ]);
+        Settings::create(['key' => 'content_card_top', 'value' =>'' ,'type_field'=> 'string' ]);
+        Settings::create(['key' => 'video_card_top', 'value' =>'' ,'type_field'=> 'video' ]);
+        Settings::create(['key' => 'header_card_buttom', 'value' =>'' ,'type_field'=> 'video' ]);
+        Settings::create(['key' => 'content_card_buttom', 'value' =>'' ,'type_field'=> 'video' ]);
+        Settings::create(['key' => 'longer_image', 'value' =>'' ,'type_field'=> 'images' ]);
+        Settings::create(['key' => 'short_image_top', 'value' =>'' ,'type_field'=> 'images' ]);
+        Settings::create(['key' => 'short_image_buttom', 'value' =>'' ,'type_field'=> 'images' ]);
+        Settings::create(['key' => 'privacy_policy', 'value' =>'' ,'type_field'=> 'string' ]);
+        Settings::create(['key' => 'Terms_Conditions', 'value' =>'' ,'type_field'=> 'string' ]);
+
     }
 }
